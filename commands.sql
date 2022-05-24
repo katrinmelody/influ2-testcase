0ff73ceab5f837ae58a04125e8f715326a73bee9
Kateryna Hutyryak <77362916+katrinmelody@users.noreply.github.com>
2022-05-24 15:51:01 +0300

    exclude additional from_stages

--- commands.sql
+++ commands.sql
@@ -1,13 +1,13 @@
 --  Counting all unique active deals
 SELECT COUNT(DISTINCT deal_id) FROM sample
-WHERE from_stage NOT IN ('Closed Lost','closedlost', 'Closed Won','Closed Won - QA', 'Dead - No Decision', 'Dead - No opportunity');
+WHERE from_stage NOT IN ('Closed Lost','closedlost', 'Closed Won','Closed Won - QA', 'Dead - No Decision', 'Dead - No opportunity', 'Closed Booked', 'Closed - Not pursued', 'Closed - Lost to competitor');
 
 
 -- how many active unique deals were in campaigns
 SELECT COUNT(DISTINCT deal_id) FROM sample
 WHERE _people_targeted > 0
 AND sub_lead_source NOT IN ('Personal Network', 'Investor Referral', 'Met at Event', 'Customer Referral', 'Direct traffic', 'Organic search', 'Marketplace Partner', 'Public Info', 'Referring Websites', 'Social media', 'Sales Partner')
-AND from_stage NOT IN ('Closed Lost','closedlost', 'Closed Won','Closed Won - QA', 'Dead - No Decision', 'Dead - No opportunity');
+AND from_stage NOT IN ('Closed Lost','closedlost', 'Closed Won','Closed Won - QA', 'Dead - No Decision', 'Dead - No opportunity', 'Closed Booked', 'Closed - Not pursued', 'Closed - Lost to competitor');
 
 
 -- Unique deals that were influenced by Influ2 according to my criteria
